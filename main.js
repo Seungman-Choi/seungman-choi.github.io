db9b652f45a3389215333789d501d07193cc45b8
Seungman-Choi <87984418+Seungman-Choi@users.noreply.github.com>
2021-08-05 02:00:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 const nav = document.querySelector('.navbar');
+let pt = false;
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
