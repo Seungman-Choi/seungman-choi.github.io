41e4d11ca7d7839e7c6dd939a1ab33ae2dcf9889
Seungman-Choi <87984418+Seungman-Choi@users.noreply.github.com>
2021-08-05 00:32:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-const nav = document.querySelector('.navbar');
-toggleBtn.addEventListener('click', () => {
+// const toggleBtn = document.querySelector('.navbar__toggleBtn');
+// const menu = document.querySelector('.navbar__menu');
+// const icons = document.querySelector('.navbar__icons');
+// const nav = document.querySelector('.navbar');
+// toggleBtn.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   icons.classList.toggle('active');
+//   nav.classList.toggle('active');
+// });
+// 20210805 change
+
+const toggleBtn = document.getElementById('navbar__toggleBtn');
+const menu = document.getElementById('.navbar__menu');
+const icons = document.getElementById('.navbar__icons');
+const nav = document.getElementById('.navbar');
+
+document.onclick = function(e){
+  if(e.target.id !=='menu' && e.target.id !=='toggleBtn')
+  {menu.classList.remove('active');
+  icon.classList.remove('active');
+  nav.classList.remove('active');}
+
+toggleBtn.onclick = function(){
+  toggleBtn.classList.toggle('active');
   menu.classList.toggle('active');
-  icons.classList.toggle('active');
-  nav.classList.toggle('active');
-});
+  icon.classList.toggle('active');
+  nav.classList.toggle('active');}
+    
+    
